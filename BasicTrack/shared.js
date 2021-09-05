f3317e9e28d1426b80e14450b3897044b8d76c4b
ScorchingShade <ankushors789@gmail.com>
2021-09-05 16:15:23 +0530

    added viewport, completed a lot of CSS

--- BasicTrack/shared.js
+++ BasicTrack/shared.js
@@ -6,24 +6,57 @@ var modal_button_no=document.querySelector('.modal__action--negative');
 
 var selectPlanButton=document.querySelectorAll('.plan button');
 
+var toggleButton = document.querySelector('.toggle-button');
+
+var mobileNav = document.querySelector('.mobile-nav');
+
+
+
 const closeModal = ()=>{
 
-    modal.style.display='none';
-    backdrop.style.display='none';
+    // modal.style.display='none';
+    // backdrop.style.display='none';
+
+    if(modal){
+        modal.classList.remove('open');
+    }
+    
+    background.classList.remove('open');
 }
 
 
+if(modal_button_no){
+    modal_button_no.addEventListener('click', closeModal);
+}
+
 selectPlanButton.forEach(i=>{
     i.addEventListener('click',()=>{
-        modal.style.display='block';
-        backdrop.style.display='block'
+        // modal.style.display='block';
+        // this.closeModal();
+
+        // modal.className='open'; This will actually overwrite a complete class listing
+        modal.classList.add('open');
+
+
+
     })
 })
 
 
-modal_button_no.addEventListener('click', closeModal);
 
-backdrop.addEventListener('click',closeModal);
+
+backdrop.addEventListener('click',()=>{
+    
+    mobileNav.style.display='none';
+    backdrop.style.display='none';
+   
+});
+
+
+toggleButton.addEventListener('click',()=>{
+    mobileNav.style.display='block';
+    backdrop.style.display='block';
+})
 
 
 //console.log(selectPlanButton)
